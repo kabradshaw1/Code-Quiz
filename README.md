0caeda355b5d6cb036ef2d0b302d50e44db85a8c
kabradshaw1 <itllbok82@gmail.com>
2022-08-24 22:15:44 -0400

    before submitting

---
+++ README.md
@@ -0,0 +1,4 @@
+## Code Quiz
+
+# Deployed Site
+
---
+++ assets/js/scripts.js
@@ -0,0 +1,29 @@
+var questions = [
+    {
+        "question": "Inside which HTML element do we put the JavaScript?",
+        "answer": ["<js>", "<scripting>", "<script>", "<javascript>"],
+        "currect": "<script>"
+    },
+    {
+        "question": "Where is the correct place to insert a JavaScript?",
+        "answer": ["Both the <head> section and the <body> section are correct", "The <body> section", "the <head> section"],
+        "correct": "Both the <head> section and the <body> section are correct"
+    },
+    {
+        "question": 'What is the correct syntax for referring to an external script called "xxx.js"?',
+        "answer": ['<scipt href="xxx.js', '<script src="xxx.js"', '<script name="xxx.js"'],
+        "correct": '<script src="xxx.js"'
+    },
+    {
+        "question": "The external JavaScript file must contain the <script> tag.",
+        "answer": [true, false],
+        "correct": false
+    },
+    // {
+    //     "question": ,
+    //     "answer": [],
+    //     "correct": 
+    // },
+
+];
+
